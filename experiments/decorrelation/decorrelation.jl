cf99cf75d2396e31b83109f3af29e5d954d2d042
wgalloway7 <20wsg02@gmail.com>
2025-02-17 12:20:07 +0000

    decorrelation investigated

--- experiments/decorrelation/decorrelation.jl
+++ experiments/decorrelation/decorrelation.jl
@@ -4,37 +4,52 @@
 using Random
 using Plots
 using Statistics
-include("lattice.jl")
-include("montecarlo.jl")
+using Dates
+include("../../core/lattice.jl")
+include("../../core/montecarlo.jl")
+include("../../core/main.jl")
 
-N = 10
-lattice = Lattice(N)
 
-beta_values = 1 ./ generate_T_intervals(10.0, 0.05, 100)
-k_values = [1]
-m = 10  # Number of runs to average over
 
-function figure_n_correlation(lattice::Lattice, beta_values::Vector{Float64}, k_values::Vector{Int64}, m::Int64, filename::String, N::Int64)
-    colors = cgrad(:RdBu, length(k_values))
+function figure_n_correlation(lattice::Lattice, beta_values::Vector{Float64}, copies::Int64, filename::String, N::Int64, monte_carlo_steps::Int64)
+    # currently takes the average of m runs
+    # but generate_decorrelation_n now does that internally
     p = plot()
-    plot!(p, background_color="#333333", gridcolor=:white, legend=:topright)
+    #plot!(p, background_color="#333333", gridcolor=:white, legend=:topright)
+    avg_correlation = generate_decorrelation_n(lattice, beta_values; k=1, move="single flip", maximum_iterations=monte_carlo_steps * N^2, copies = copies)
+    plot!(p, beta_values, log10.(avg_correlation))
     
-    for (i, k) in enumerate(k_values)
-        all_correlations = []
-        
-        for _ in 1:m
-            push!(all_correlations, generate_decorrelation_n(lattice, beta_values; k=k, move="single flip", maximum_iterations=10000))
-        end
-        
-        avg_correlation = mean(all_correlations)
-        plot!(p, beta_values, avg_correlation, label="k = $k", color=colors[i])
-    end
-    
-    xlabel!(p, "Beta", xlabelcolor=:white)
-    ylabel!(p, "Average n_correlation", ylabelcolor=:white)
-    title!(p, "Average n_correlation vs Beta for various k, N = $N, number of runs = $m", titlecolor=:white)
+    xlabel!(p, "Beta")
+    ylabel!(p, "log(Average n_correlation)")
+    title!(p, "N = $N, $copies runs, $monte_carlo_steps  Monte Carlo steps")
     savefig(p, filename)
 end
 
-filename = "decorrelation_avg"
-figure_n_correlation(lattice, beta_values, k_values, m, filename, N)
+copies = 5
+beta_values = 1 ./ generate_T_intervals(10.0, 0.5, 100)
+N_values = [10,20,50,100]
+monte_carlo_steps = 10
+lattice = Lattice(10)
+for N in N_values
+    time = now()
+    println(N)
+    lattice.N = N
+    lattice.grid = solved_configuration(N)
+    filename = joinpath("experiments","decorrelation","decorrelation_avg_N$N.png")
+    figure_n_correlation(lattice, beta_values, copies, filename, N, monte_carlo_steps)
+    println(now() - time)
+    
+end
+
+monte_carlo_steps_values = [1,2,5,10,20]
+lattice.N = 50
+lattice.grid = solved_configuration(50)
+for m in monte_carlo_steps_values
+    time = now()
+    println(m)
+    filename = joinpath("experiments","decorrelation","decorrelation_avg_m$m.png")
+    figure_n_correlation(lattice, beta_values, copies, filename, 50, m)
+    println(now() - time)
+end
+    
+    
