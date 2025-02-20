286928a83cedc5a76d6f26b400dd4c0e44476c18
Will Galloway <wsg22@pc65.tcm.phy.cam.ac.uk>
2025-02-20 21:28:49 +0000

    saddles run

--- experiments/saddles/saddles.jl
+++ experiments/saddles/saddles.jl
@@ -4,7 +4,6 @@ using Plots
 using Statistics
 using DelimitedFiles
 using Dates
-using Profile, ProfileView
 
 include("../../core/lattice.jl")
 include("../../core/montecarlo.jl")
@@ -16,16 +15,16 @@ lattice.grid = solved_configuration(N)
 beta_values = 1 ./ generate_T_intervals(10.0, 0.25, 200)
 
 
-datafile = "saddles.csv"
-folder = "experiments\\saddles"
+datafile = "saddles_singleflip.csv"
+folder = ""
 time = now()
-results = generate_saddles(lattice, beta_values, 5, "k chain flip",20)
+results = generate_saddles(lattice, beta_values, 5, "single flip",20)
 writedlm(joinpath(folder, datafile), results, ',')
 println(now() - time)
 p1 = plot()
 scatter!(p1, results[2], results[1], markerstrokedwidth = 1, markersize = 1, label = "")
-savefig(p1, joinpath(folder, "saddles.png"))
+savefig(p1, joinpath(folder, "saddles_singleflip.png"))
 
 p2 = plot()
 scatter!(p2, results[3], results[1], markerstrokedwidth = 1, markersize = 1, label = "")
-savefig(p2, joinpath(folder, "saddles_beta.png"))
+savefig(p2, joinpath(folder, "saddles_singleflip_beta.png"))
